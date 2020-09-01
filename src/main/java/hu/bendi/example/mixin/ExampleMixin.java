ac87f4ce3f0066c1c689002a4ada4a36330e8a3b
Bendimester23 <58264102+Bendimester23@users.noreply.github.com>
2020-09-01 16:55:52 +0200

    dsgfdg

--- src/main/java/hu/bendi/example/mixin/ExampleMixin.java
+++ src/main/java/hu/bendi/example/mixin/ExampleMixin.java
@@ -1,6 +1,13 @@
 package hu.bendi.example.mixin;
 
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import net.minecraft.text.Texts;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,14 +15,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public abstract class ExampleMixin extends Screen {
+
+	protected ExampleMixin(Text title) {
+		super(title);
+	}
+
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
-		//Collection<String> strs = new ArrayList<>();
-		//strs.add("asfdsg");
-		//this.addButton(new ButtonWidget(100, 100, 100, 100, Texts.joinOrdered(strs), (asd) -> {
-		//	System.out.println("fg9hfthoo");
-		//}));
+		Collection<String> strs = new ArrayList<>();
+		strs.add("asfdsg");
+		this.addButton(new ButtonWidget(100, 100, 100, 20, Texts.joinOrdered(strs), (asd) -> {
+			System.out.println("fg9hfthoo");
+		}));
 	}
 }
